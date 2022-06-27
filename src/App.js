ac852c8569a4ed018e4a15cb47da0ca78bbf0910
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-06-28 05:35:43 +0600

    added SCSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
 import { test } from "./reduxToolkit/toolkitReducer";
+import "./App.scss";
 function App() {
   const testShowCount = useSelector((state) => state.toolkit.count);
   const dispatch = useDispatch();
   return (
     <div>
       <div>{testShowCount}</div>
-      <button onClick={() => dispatch(test())}>click here</button>
+      <button className="btn" onClick={() => dispatch(test())}>
+        click here
+      </button>
     </div>
   );
 }
