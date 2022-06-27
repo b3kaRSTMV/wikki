c2ef43ea43da3a1b4e47c4c720b3996f36f1d3a3
Esenushka <prosterling.mien@gmail.com>
2022-06-27 18:30:13 +0600

    fix: eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {},
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  env: {
+    browser: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  rules: {
+    "no-console": "off"
+  }
+};
