3ad046012c5219ec9a6d941fdb613e6460b81365
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-06-28 09:17:02 +0600

    feat: added new folder and new files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 import { test } from "./reduxToolkit/toolkitReducer";
-import "./App.scss";
 function App() {
   const testShowCount = useSelector((state) => state.toolkit.count);
   const dispatch = useDispatch();
   return (
     <div>
       <div>{testShowCount}</div>
-      <button className="btn" onClick={() => dispatch(test())}>
-        click here
-      </button>
+      <button onClick={() => dispatch(test())}>click here</button>
     </div>
   );
 }
