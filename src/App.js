01690b467fc372630641792e4d861ad805ef5259
bektur14 <bektur.bnb9@gmail.com>
2022-07-01 02:44:02 +0600

    set-up i18-next

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { useSelector, useDispatch } from "react-redux";
 import { test } from "./reduxToolkit/toolkitReducer";
+
+import { useTranslation } from "react-i18next";
+
 function App() {
+  const { t } = useTranslation();
   const testShowCount = useSelector((state) => state.toolkit.count);
   const dispatch = useDispatch();
   return (
     <div>
       <div>{testShowCount}</div>
       <button onClick={() => dispatch(test())}>click here</button>
+      <h1>{t("Welcome")}</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,27 @@ import { store } from "./reduxToolkit";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ["en", "ru"],
+    fallbackLng: "en",
+    debug: false,
+    detection: {
+      order: ["path", "cookie", "htmlTag"],
+      caches: ["cookie"]
+    },
+    backend: {
+      loadPath: "/shared/locales/{{lng}}/translation.json"
+    }
+  });
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
