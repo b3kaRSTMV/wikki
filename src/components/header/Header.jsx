d47d56f4b431f5c24b91e469fdb9d13d9a60385e
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-07-03 08:09:10 +0600

    feat: added burger

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
 import css from "./Header.module.scss";
 
 export default function Header() {
   return (
     <div className="container">
-      <div className={css.wrapper}>
-        <img src={logo} alt="something went wrong"></img>
-        <div className={css.a_container}>
-          <a href="#" className={css.header_link}>
-            Features
-          </a>
-          <a href="#" className={css.header_link}>
-            Sign Up
-          </a>
-          <a href="#" className={css.link_with_background}>
-            Sign In
-          </a>
+      <div className={css.header}>
+        <div className={css.header_body}>
+          <Link to="/" className={css.logo}>
+            <img src={logo} alt="something went wrong" />
+          </Link>
+          <div className={css.header_burger}>
+            <span></span>
+          </div>
+          <nav className={css.header_menu}>
+            <ul className={css.header_list}>
+              <li>
+                <Link to="/" className={css.header_link}>
+                  Features
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className={css.header_link}>
+                  Sign Up
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className={css.header_link_with_background}>
+                  Sign In
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
+        <hr className={css.hr}></hr>
       </div>
-      <hr className={css.hr}></hr>
     </div>
   );
 }
