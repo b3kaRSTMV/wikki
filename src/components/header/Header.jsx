8b2d50689d4c4ab98d42da53107cb6e5218075e5
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-07-04 11:01:38 +0600

    fix: fixed some issues with the burger menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
 import css from "./Header.module.scss";
-
+import { useState } from "react";
 export default function Header() {
+  const [active, setActive] = useState(false);
   return (
     <div className="container">
       <div className={css.header}>
         <div className={css.header_body}>
           <Link to="/" className={css.logo}>
-            <img src={logo} alt="something went wrong" />
+            <img src={logo} alt="Logo" />
           </Link>
-          <div className={css.header_burger}>
+          <div className={css.header_burger} onClick={() => setActive(!active)}>
             <span></span>
           </div>
           <nav className={css.header_menu}>
@@ -36,6 +37,27 @@ export default function Header() {
         </div>
         <hr className={css.hr}></hr>
       </div>
+      {active && (
+        <nav className={css.header_menu_active}>
+          <ul className={css.header_list_active}>
+            <li>
+              <Link to="/" className={css.header_link}>
+                Features
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className={css.header_link}>
+                Sign Up
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className={css.header_link_with_background}>
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 }
