9ee163772860fd45a8ee3db5511dad2457a829e7
bektur14 <bektur.bnb9@gmail.com>
2022-07-05 09:25:21 +0600

    add i18next

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import css from "../footer/Footer.module.scss";
+import { useTranslation } from "react-i18next";
+
 function Footer() {
+  const { t } = useTranslation();
   return (
     <>
       <footer>
@@ -8,26 +11,27 @@ function Footer() {
           <div className={css.footer__items}>
             <nav className={css.footer__items_links}>
               <div>
-                <h4>Products</h4>
-                <Link to="/features">Features</Link>
-                <Link to="/pricing">Pricing</Link>
+                <h4>{t("Products")}</h4>
+                <Link to="/features">{t("Features")}</Link>
+                <Link to="/pricing">{t("Pricing")}</Link>
               </div>
               <div>
-                <h4>Social</h4>
-                <Link to="/twitter">Twitter</Link>
-                <Link to="/facebook">Facebook</Link>
-                <Link to="/facebook">Contact Us</Link>
+                <h4>{t("Social")}</h4>
+                <Link to="/twitter">{t("Twitter")}</Link>
+                <Link to="/facebook">{t("Facebook")}</Link>
+                <Link to="/facebook">{t("Contact Us")}</Link>
               </div>
               <div>
-                <h4>Legal</h4>
-                <Link to="/privacy">Privacy</Link>
-                <Link to="/privacy">Terms</Link>
+                <h4>{t("Legal")}</h4>
+                <Link to="/privacy">{t("Privacy")}</Link>
+                <Link to="/privacy">{t("Terms")}</Link>
               </div>
             </nav>
             <div className={css.footer__logo}>
               <img src="./shared/img/logo.png" alt="logo" />
               <p>
-                Created by awesome people <br /> at <span>Oozou</span>
+                {t("Created by awesome people")}
+                <br /> {t("at")} <span>{t("Oozou")}</span>
               </p>
             </div>
           </div>
---
+++ src/i18next.js
@@ -0,0 +1,22 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ["en", "ru"],
+    fallbackLng: "en",
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ["path", "cookie", "htmlTag"],
+      caches: ["cookie"]
+    },
+    backend: {
+      loadPath: "/shared/locales/{{lng}}/translation.json"
+    }
+  });
