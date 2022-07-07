f9d78bb532d5fc2b2cdddac2a410717f9f4c92b1
bektur14 <bektur.bnb9@gmail.com>
2022-07-07 18:26:32 +0600

    added container

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import Header from "../components/header/Header";
+import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 export default function MainPage() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Footer />
     </>
   );
