347ab4018c73bdf393b48fcefa2cc8862ade6e65
bektur14 <bektur.bnb9@gmail.com>
2022-07-13 16:15:24 +0600

    fixed

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import css from "./loginForm.module.scss";
+import css from "./LoginPage.module.scss";
 import { useTranslation } from "react-i18next";
 import "../../i18next";
 
-function LoginForm() {
+function LoginPage() {
   const { t } = useTranslation();
   return (
     <div className="container">
@@ -12,41 +12,41 @@ function LoginForm() {
         <div className={css.login__links}>
           <Link to="/" className={css.link__facebook}>
             <img src="./shared/img/facebook.svg" alt="" />
-            <span>{t("Login with facebook")}</span>
+            <span>{t("loginPage.facebook")}</span>
           </Link>
           <Link to="/" className={css.link__github}>
             <img src="./shared/img/git.svg" alt="" />
-            <span>{t("Sign in with github")}</span>
+            <span>{t("loginPage.github")}</span>
           </Link>
           <Link to="/" className={css.link__google}>
             <img src="./shared/img/google.svg" alt="" />
-            <span>{t("Sign in with google")}</span>
+            <span>{t("loginPage.google")}</span>
           </Link>
         </div>
         <div className={css.line}>
-          <span>{t("OR")}</span>
+          <span>{t("loginPage.OR")}</span>
         </div>
         <form action="#">
           <div class={css.control}>
-            <label for="email">{t("Email")}</label>
+            <label for="email">{t("loginPage.Email")}</label>
             <input type="email" name="email" className={css.control__input} />
           </div>
           <div class={css.control}>
-            <label for="psw">{t("Password")}</label>
+            <label for="psw">{t("loginPage.Password")}</label>
             <input type="password" name="psw" className={css.control__input} />
           </div>
           <div class={css.login__submit}>
             <div>
               <input type="checkbox" />
-              <span className={css.login__text}>{t("Remember me")}</span>
+              <span className={css.login__text}>{t("loginPage.Remember me")}</span>
             </div>
             <Link to="/" className={css.login__text}>
-              {t("Forgot a password?")}
+              {t("loginPage.password?")}
             </Link>
           </div>
           <div class={css.control}>
             <a href="" className={css.control__sign}>
-              {t("SIGN IN")}
+              {t("loginPage.SIGN IN")}
             </a>
           </div>
         </form>
@@ -55,4 +55,4 @@ function LoginForm() {
   );
 }
 
-export default LoginForm;
+export default LoginPage;
