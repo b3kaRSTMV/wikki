d66c456cdd3abb5ec76e349f3448989d4dc744a9
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-07-23 02:24:42 +0600

    feat: added authorization with github,facebook, google and did some things with firebase

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -2,9 +2,19 @@ import css from "./Register.module.scss";
 import { useTranslation } from "react-i18next";
 import "../../i18next";
 import { useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  signInWithPopup,
+  GithubAuthProvider
+} from "firebase/auth";
+// import { firestore } from "firebase/firestore";
 import { useDispatch } from "react-redux/es/exports";
 import { setUser } from "../../reduxToolkit/slices/userSlice";
+import { useAuth } from "../../hooks/useAuth";
+import { Navigate } from "react-router-dom";
 
 const Register = () => {
   const { t } = useTranslation();
@@ -14,6 +24,7 @@ const Register = () => {
   const [companyName, setCompanyName] = useState("");
   const [name, setName] = useState("");
   const auth = getAuth();
+  const { isAuth } = useAuth();
   const handleSignUp = (e) => {
     createUserWithEmailAndPassword(auth, email, pass, companyName, name)
       .then(({ user }) => {
@@ -25,9 +36,65 @@ const Register = () => {
             token: user.accessToken
           })
         );
+        setTimeout(redirect, 10000);
       })
       .catch(console.error);
     e.preventDefault();
+  };
+
+  function redirect() {
+    if (isAuth === true) {
+      <Navigate to="/login" />;
+    } else {
+      alert("тебе нужно зарегистрироваться");
+    }
+  }
+
+  const googleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider)
+      .then(({ user }) => {
+        console.log(user);
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.accessToken
+          })
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const facebookLogin = async () => {
+    const provider = new FacebookAuthProvider();
+    await signInWithPopup(auth, provider)
+      .then(({ user }) => {
+        console.log(user);
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.accessToken
+          })
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const githubLogin = async () => {
+    const provider = new GithubAuthProvider();
+    await signInWithPopup(auth, provider)
+      .then(({ user }) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className={css.wrapper}>
@@ -38,15 +105,15 @@ const Register = () => {
             <p className={css.main_text}>{t("register.main_text")}</p>
           </div>
           <div className={css.social_networks_container}>
-            <div className={css.facebook}>
+            <div className={css.facebook} onClick={facebookLogin}>
               <img className={css.icon} src="/images/register/facebook.png" alt="logo" />
               <h3 className={css.social_networks_text}>{t("register.facebook")}</h3>
             </div>
-            <div className={css.github}>
+            <div className={css.github} onClick={githubLogin}>
               <img className={css.icon} src="/images/register/github.png" alt="logo" />
               <h3 className={css.social_networks_text}>{t("register.github")}</h3>
             </div>
-            <div className={css.google}>
+            <div className={css.google} onClick={googleLogin}>
               <img className={css.icon} src="/images/register/google.png" alt="logo" />
               <h3 className={css.social_networks_text}>{t("register.google")}</h3>
             </div>
